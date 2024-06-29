92114e769cf82b3d1f76d8740b48838a8fe7d260
Gabriela Nery <gneri94@gmail.com>
2024-06-28 21:14:28 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+// Carregar variáveis de ambiente
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(morgan('combined', { stream: accessLogStream }))
+
+// Middlewares
+app.use(express.json());
+
+// Rota de exemplo
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+// Tratamento de erros
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Algo deu errado!');
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
